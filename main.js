d3f10292b347e6ba2c04d279c0caf32f47ed75c6
max <89175221282@mail.ru>
2023-05-30 20:16:11 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,71 @@
+let min = document.querySelector(`.min`);  
+let sec = document.querySelector(`.sec`);
+    
+let sec1 = 60;
+let min1 = 3;
+/*
+
+min.innerHTML =`0${min1}`;
+for (let index = 0; index < 3; index++) {
+    function turnMin() {
+        if (min1<-1) {
+            min1--;
+            min.innerHTML =`0${min1}`;            
+            console.log(min1);
+            
+        };
+        x++;
+        };
+};
+    setInterval(turnMin, 6000);
+for (let index = 0; index < 3; index++) {
+    
+    
+} 
+function click() {
+    
+}
+click()
+/* 
+setInterval(click, 1000)
+  */   
+ min.innerHTML =`0${min1}`;
+for (let index = 0; index < 60; index++) {
+        
+    function turnMin() {
+        if (min1<1) {
+            return min1 =3;
+            
+        } else {
+                min1--;
+                min.innerHTML =`0${min1}`;            
+            } 
+        };
+            };
+            
+                
+
+setInterval(turnMin, 60000);
+
+
+
+    for (let index = 0; index < 60; index++) {
+        
+        function turnSec() {
+            if (sec1<1) {
+                return sec1 =60;
+                
+            } else {
+                if (sec1<11) {
+                    sec1--;
+                    sec.innerHTML =`0${sec1}`;            
+                } else {
+                    sec1--;
+                    sec.innerHTML = sec1;    
+                };
+            };
+                };
+                
+            };        
+
+setInterval(turnSec, 1000);
